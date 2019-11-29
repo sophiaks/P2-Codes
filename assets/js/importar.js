a828edec5c15c591208cace1f3d60aeffca611dc
Fernando Sakabe <fernandokenjisakabe@gmail.com>
2019-11-29 09:24:26 -0300

    alterando fonte

--- assets/js/importar.js
+++ assets/js/importar.js
@@ -19,7 +19,7 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 var dbRef = firebase.database();
-var contactsRef2 = dbRef.ref('/usuario/frontend/candidatos/Bilbia88');
+var contactsRef2 = dbRef.ref('/usuario/frontend/candidatos/Bilbia99');
 contactsRef2.set({
   
       teste:{
